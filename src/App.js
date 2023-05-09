5ca1854368006be359e01d2ff11cecaa6c462126
Abhaykumar47 <abhaykumar8550@gmail.com>
2023-05-09 10:50:38 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, Suspense, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { Bounds, OrbitControls, PerformanceMonitor, Stage } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { useAnimations } from '@react-three/drei';
+
+
+function Model({ modelUri }) {
+  const group = useRef();
+  const { scene, animations } = useLoader(GLTFLoader, modelUri);
+  const { actions, mixer } = useAnimations(animations, group.current);
+  
+  useEffect(() => {
+    actions?.Animation?.play();
+  }, [mixer, actions]);
+
+  return <primitive ref={group} object={scene} dispose={null} />;
+}
+
+function ModelUploader({ onModelUpload }) {
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const modelUri = e.target.result;
+      onModelUpload(modelUri);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  return <input type="file" accept=".glb" onChange={handleFileUpload} />;
+}
 
 function App() {
+  const [modelUri, setModelUri] = useState(null);
+
+  const handleModelUpload = (uri) => {
+    setModelUri(uri);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {modelUri ? (
+        <Canvas
+          id={modelUri}
+          dpr={1}
+          style={{ height: '100vh' }}
+          camera={{ fov: 10 }}
         >
-          Learn React
-        </a>
-      </header>
+          <PerformanceMonitor>
+            <pointLight color="white" intensity={1} position={[10, 10, 10]} />
+            <Suspense fallback={null}>
+              <Stage adjustCamera={false} intensity={0.6} shadows={false}>
+                <Bounds fit clip>
+                  <Model modelUri={modelUri} />
+                </Bounds>
+              </Stage>
+            </Suspense>
+          </PerformanceMonitor>
+          <OrbitControls />
+        </Canvas>
+      ) : (
+        <ModelUploader onModelUpload={handleModelUpload} />
+      )}
     </div>
   );
 }
